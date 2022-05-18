b64cd36671b36e2a81e89412bac0afb4c5221907
nnhungjs <nnhungjs@gmail.com>
2022-05-18 08:30:50 +0700

    fix: config babel

---
+++ src/advanced-guides/code-spliting.js
@@ -0,0 +1,13 @@
+// import math from './math'
+
+const DiceRoll = () => {
+
+  return (
+    <div>
+      Your dice roll: aida
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DiceRoll />)
